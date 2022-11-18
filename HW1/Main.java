f372fdd5e84c33421e796f114aae0cfa6ef14809
Marat-Tim <rurikcat@gmail.com>
2022-11-18 22:42:07 +0300

    Сделал разделение папок по домашним заданиям

--- HW1/Main.java
+++ HW1/Main.java
@@ -1,26 +1,26 @@
 public class Main {
-    public static void printlnArray(int[] array) {
-        for (int element: array) {
-            System.out.print(element + " ");
-        }
-        System.out.println();
-    }
-
     public static void bubbleSort(int[] array) {
-        boolean is_sorted = false;
+        boolean isSorted = false;
         int i = array.length - 1;
-        while (!is_sorted) {
-            is_sorted = true;
+        while (!isSorted) {
+            isSorted = true;
             for (int j = 0; j < i; j++) {
                 if (array[j] > array[j + 1]) {
                     array[j] ^= array[j + 1];
                     array[j + 1] ^= array[j];
                     array[j] ^= array[j + 1];
-                    is_sorted = false;
+                    isSorted = false;
                 }
             }
             i--;
         }
+    }
+
+    public static void printlnArray(int[] array) {
+        for (int element: array) {
+            System.out.print(element + " ");
+        }
+        System.out.println();
     }
 
     public static void main(String[] args) {
